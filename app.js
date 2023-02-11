bbce8b8d72d5fcce2acb55fd0630c660b9c8846d
Alexander Chaschin <ac@happynumbers.com>
2023-02-11 19:58:56 +0300

    extract signal

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import 'preact/debug';
 import { render } from 'preact';
-import { useSignal } from '@preact/signals';
+import { signal } from '@preact/signals';
 import { html } from './html.js';
 
-function App() {
-  const count = useSignal(0);
-  const increment = () => { console.log(count); count.value++; }
+const count = signal(0);
+const increment = () => { count.value++; }
 
+function App() {
   return html`
     <div>
       <h1>${count.value}</h1>
