2927b1b739898b5ee1be1388688931383845e482
Alexander Chaschin <ac@happynumbers.com>
2023-02-12 07:49:05 +0300

    add set width

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import 'preact/debug';
 import { render } from 'preact';
 import { html } from './html.js';
-import { images, field, fieldSize, setHeight } from './state.js';
+import { images, field, fieldSize, setHeight, setWidth } from './state.js';
 
 function FieldControls() {
   const { width, height } = fieldSize.value;
@@ -9,10 +9,11 @@ function FieldControls() {
   return html`
     <div class='fieldControls'>
       <label class='fieldLabel' for='width'>Width: </label>
-      <input class='fieldInput' type='number' name='width' value=${width} min='1'
+      <input class='fieldInput' type='number' name='width' value=${width} min='1' max='25'
+        onchange=${(e) => setWidth(e.target.value)}
       />
       <label class='fieldLabel' for='height'>Height: </label>
-      <input class='fieldInput' type='number' name='height' value=${height} min='1'
+      <input class='fieldInput' type='number' name='height' value=${height} min='1' max='25'
         onchange=${(e) => setHeight(e.target.value)}
       />
     </div>
--- state.js
+++ state.js
@@ -18,6 +18,22 @@ export const fieldSize = computed(() => ({
   width: field.value[0].length,
 }));
 
+export function setWidth(newWidth) {
+  const { width, height } = fieldSize.value;
+  if(newWidth <= 0) return;
+
+  if(newWidth < width) {
+    field.value = field.value.map(row => row.slice(0, newWidth));
+  } else if(newWidth > width) {
+    const newCount = newWidth - width;
+    field.value = field.value.map(row => {
+      const newCells = Array(newCount).fill(background.value);
+      return [...row, ...newCells];
+    });
+  }
+}
+
+
 export function setHeight(newHeight) {
   const { width, height } = fieldSize.value;
   if(newHeight <= 0) return;
